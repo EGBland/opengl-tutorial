00ffbe019e95620a2380da2260a2c1543ccb4a7a
E. G. Bland <egb528@york.ac.uk>
2021-06-10 12:58:07 +0100

    complete "hello triangle" tutorial

--- OpenGLTutorial/Main.cpp
+++ OpenGLTutorial/Main.cpp
@@ -2,6 +2,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// init
+void init();
+
 // for glad framebuffer size callback
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -11,12 +14,34 @@ void processInput(GLFWwindow* window);
 // render function for main loop
 void render(GLFWwindow* window);
 
+// some data
+const float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	-0.5f,  0.5f, 0.0f,
+	 0.5f,  0.5f, 0.0f
+};
+
+const unsigned int indices[] = {
+	0, 1, 2,
+	1, 2, 3
+};
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"\tgl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"\tFragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\0";
+
 int main() {
 	// init
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	init();
 
 	// make a window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "My OpenGL Window", NULL, NULL);
@@ -39,6 +64,40 @@ int main() {
 	// give glad another frame buffer size callback
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// make vao
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// make vbo
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// make ebo
+	unsigned int ebo;
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// compile shaders
+	unsigned int vertexShader, fragShader, shaderProgram;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+	glCompileShader(fragShader);
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragShader);
+	glLinkProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragShader);
+
 	// main loop
 	while (!glfwWindowShouldClose(window)) {
 		// process
@@ -46,6 +105,11 @@ int main() {
 
 		// render
 		render(window);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 
 		// glfw
 		glfwSwapBuffers(window);
@@ -55,6 +119,13 @@ int main() {
 	// the end
 	glfwTerminate();
 	return 0;
+}
+
+void init() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
